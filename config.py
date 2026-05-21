77c48f4876bc62d91559cfe97cc1e242e5192eb9
nsgaming611-lab <nsgaming611@gmail.com>
2026-05-21 22:11:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("37714867", ""))
